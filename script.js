669f45933d418176173ebca8541d223da9dad7b7
yatemmma <yatemmma@gmail.com>
2015-11-15 21:42:55 +0900

    init

---
+++ script.js
@@ -0,0 +1,59 @@
+var _name = 'slide';
+var _currentPageIndex = 0;
+var _contents = [];
+
+$(function() {
+  $(window).keyup(function(e) {
+    switch (e.keyCode) {
+      case 37: // left
+      case 38: // up
+        prev();
+        break;
+      case 39: // right
+      case 40: // down
+        next();
+        break;
+    }
+  });
+  require([_name+'/contents'], function(contents) {
+    _contents = contents();
+    showPage(_currentPageIndex);
+  });
+});
+
+function prev() {
+  if (_currentPageIndex <= 0) return;
+  showPage(--_currentPageIndex);
+}
+
+function next() {
+  if (_currentPageIndex >= _contents.length - 1) return;
+  showPage(++_currentPageIndex);
+}
+
+function showPage(index) {
+  var c = _contents[index];
+  $('.page').hide();
+  eval('setContent_'+c.type).call(this, c);
+  $('#'+c.type).show();
+}
+
+function setContent_title(c) {
+  var $page = $('#title');
+  $page.find('.title').text(c.title);
+}
+
+function setContent_desc(c) {
+  var $page = $('#desc');
+  $page.find('.title').text(c.title);
+  $page.find('.desc').text(c.desc);
+}
+
+function setContent_list(c) {
+  var $page = $('#list');
+  $page.find('.title').text(c.title);
+  $page.find('.list').empty();
+  for (i in c.list) {
+    $page.find('.list').append($('<li>').text(c.list[i]));
+  }
+}
---
+++ slide/contents.js
@@ -0,0 +1,10 @@
+define(function() {
+  return function() {
+    var contents = [
+        {type: 'title', title: 'My Aowsome Slide'},
+        {type: 'desc', title: 'Sliderify', desc: 'This is Sliderify.'},
+        {type: 'list', title: 'Points', list: ['Simple', 'Easy', 'Beautiful']}
+    ];
+    return contents;
+  }
+});
