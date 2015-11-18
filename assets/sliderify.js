793cd27496c115bd8f01081959f16853c94c4961
yatemmma <yatemmma@gmail.com>
2015-11-19 08:09:05 +0900

    wip

--- assets/sliderify.js
+++ assets/sliderify.js
@@ -1,68 +1,203 @@
-var params = getQueryStrings();
-var _name = params.slide || 'sample';
-var _currentPageIndex = 0;
-var _contents = [];
+/*
+ sliderify.js 0.0.1 Copyright (c) yatemmma.
+ Available via the MIT license.
+ see: https://github.com/yatemmma/sliderify for details
+*/
+
 var _slideConfig = null;
 
-function getQueryStrings() {
-    var url = window.location.search;
-    var items = url.slice(1).split('&');
+Function.prototype.heredoc = function() {
+  return this.toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/^\n/, "");
+};
+
+var Template = {
+  aaa: function() {/*
+<div>
+  hello: <%= name %>
+</div>
+<a>xxx</a>
+*/}
+};
+
+function Slide() {
+  var queryStrings = (function() {
+    var items = window.location.search.slice(1).split('&');
     var params = {}
     items.forEach(function(item) {
       var keyValue = item.split('=');
       params[keyValue[0]] = keyValue[1];
     });
     return params;
+  });
+
+  this.target = queryStrings.slide || 'sample';
+  this.currentIndex = queryStrings.page || 0;
+  this.pages = [];
+
+  var $link = $('<link>').attr('rel', "stylesheet")
+                         .attr('media', "all")
+                         .attr('href', this.target+'/custom.css');
+  $("head").append($link);
 }
 
-function Page() {
-  this.config = {};
-  this.items = [];
-}
-Page.prototype.setConfig = function(config) {
-  this.config = config;
-}
-Page.prototype.addItem = function(item) {
-  this.items.push(item);
-}
-Page.prototype.type = function() {
-  return this.items.map(function(item) {
-    return item[0];
-  }).join('_');
-}
+Slide.prototype.setConfig = function(item) {
+  console.log("config!"); //TODO
+};
 
-$(function() {
+Slide.prototype.setItems = function(items) {
+  this.pages = items.map(function(item) {
+  return item; //TODO
+  });
+};
+
+Slide.prototype.setKeyEvents = function() {
+  var slide = this;
   $(window).keyup(function(e) {
     switch (e.keyCode) {
       case 37: // left
       case 38: // up
-        prev();
+        slide.prev();
         break;
       case 39: // right
       case 40: // down
-        next();
+        slide.next();
         break;
     }
   });
+};
 
-  require([_name+'/document'], function(document) {
-    var $link = $('<link>');
-    $link.attr('rel',"stylesheet");
-    $link.attr('media',"all");
-    $link.attr('href',_name+'/custom.css');
-    $("head").append($link);
+Slide.prototype.prev = function() {
+  if (this.currentIndex <= 0) return;
+  this.currentIndex--;
+  this.showPage();
+};
 
-    var text = document.toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/^\n/, "");
-    console.log(markdown.parse(text));
-    console.log(markdown.parse(text).toString());
-    _contents = toJson(markdown.parse(text));
-    console.log(_contents);
-    showPage(_currentPageIndex);
+Slide.prototype.next = function() {
+  if (this.currentIndex >= this.pages.length - 1) return;
+  this.currentIndex++;
+  this.showPage();
+};
 
-    var compiled = _.template("<div>hello: <%= name %></div>");
-    $("#header").append($(compiled({name: 'momotaro'})));
+Slide.prototype.showPage = function() {
+  var page = this.pages[this.currentIndex];
+
+  var compiled = _.template(page.template().heredoc());
+  var $pageElement = $(compiled(page.data()));
+
+  $("body").empty()
+           .append($pageElement);
+};
+
+
+function Page() {
+  this.config = null;
+  this.items = [];
+}
+Page.prototype.setConfig = function(item) {
+  this.config = item; //TODO
+};
+Page.prototype.addItem = function(item) {
+  this.items.push(item); //TODO
+};
+Page.prototype.type = function() {
+  return this.items.map(function(item) {
+    return item[0];
+  }).join('_');
+};
+Page.prototype.template = function() {
+  return Template.aaa; //TODO
+};
+Page.prototype.data = function() {
+  return {name: 'momotaro'}; //TODO
+};
+Page.prototype.isConfigOnly = function() {
+  return true; //TODO
+};
+
+$(function() {
+  var slide = new Slide();
+  slide.setKeyEvents();
+
+  require([slide.target+'/text'], function(text) {
+    var intermediate = markdown.parse(text.heredoc());
+
+    var items = dividePerPages(intermediate);
+    if (!slide.config && items[0].isConfigOnly()) {
+      slide.setConfig = items.shift();
+    }
+    slide.setItems(items);
+    // console.log(intermediate);
+    console.log(slide.pages);
+    slide.showPage();
+
   });
 });
+
+function dividePerPages(intermediate) {
+  var items = [];
+  var page = new Page();
+  intermediate.shift(); // igonre first item "markdown"
+  intermediate.forEach(function(content, index) {
+    switch (content[0]) {
+      case 'hr':
+        items.push(page);
+
+        // if (!_slideConfig && page.items == 0) {
+        //   _slideConfig = page;
+        // } else {
+        //   codes.push(page);
+        // }
+        // page = new Page();
+        break;
+        console.log(content);
+      case 'para':
+        page.addItem(content);
+        // var text = content[1];
+        // if (text.charAt(0) == "{") {
+        //   page.setConfig(text);
+        // } else {
+        //   page.addItem(content);
+        // }
+        console.log(content);
+        break;
+      default:
+        page.addItem(content);
+        console.log(content);
+        break;
+    }
+  });
+  items.push(page);
+  return items;
+
+  // var page = new Page();
+  // contents.shift(); // igonre first item "markdown"
+  // contents.forEach(function(content, index) {
+  //   switch (content[0]) {
+  //     case 'hr':
+  //       if (!_slideConfig && page.items == 0) {
+  //         _slideConfig = page;
+  //       } else {
+  //         codes.push(page);
+  //       }
+  //       page = new Page();
+  //       break;
+  //     case 'para':
+  //       var text = content[1];
+  //       if (text.charAt(0) == "{") {
+  //         page.setConfig(text);
+  //       } else {
+  //         page.addItem(content);
+  //       }
+  //       break;
+  //     default:
+  //       page.addItem(content);
+  //       console.log(content);
+  //       break;
+  //   }
+  // });
+  // codes.push(page);
+  // return codes;
+}
 
 function toJson(contents) {
   var codes = [];
@@ -95,59 +230,3 @@ function toJson(contents) {
   codes.push(page);
   return codes;
 }
-
-function prev() {
-  if (_currentPageIndex <= 0) return;
-  showPage(--_currentPageIndex);
-}
-
-function next() {
-  if (_currentPageIndex >= _contents.length - 1) return;
-  showPage(++_currentPageIndex);
-}
-
-function showPage(index) {
-  var page = _contents[index];
-  $('.page').hide();
-
-  var type = page.type();
-
-  eval('setContent_'+type).call(this, page);
-  $('#'+type).show();
-}
-
-function setContent_header(page) {
-  var $page = $('#header');
-  $page.find('.header').text('xxxx');
-}
-
-// function setContent_notfound(c) {}
-//
-// function setContent_title(c) {
-//   var $page = $('#title');
-//   $page.find('.title').text(c.title);
-// }
-//
-// function setContent_desc(c) {
-//   var $page = $('#desc');
-//   $page.find('.title').text(c.title);
-//   $page.find('.desc').text(c.desc);
-// }
-//
-// function setContent_list(c) {
-//   var $page = $('#list');
-//   $page.find('.title').text(c.title);
-//   $page.find('.list').empty();
-//   for (i in c.list) {
-//     $page.find('.list').append($('<li>').text(c.list[i]));
-//   }
-// }
-//
-// function setContent_introduction(c) {
-//   var $page = $('#introduction');
-//   $page.find('.image').empty()
-//                       .append($('<img>').attr({src:_name+'/'+c.image}));
-//   $page.find('.name').text(c.name);
-//   $page.find('.account').text(c.account);
-//   $page.find('.desc').text(c.desc);
-// }
