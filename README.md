6f57438df6bf2c20d3da01f11830ce1047cd5e74
yatemmma <yatemmma@gmail.com>
2016-09-25 05:12:55 +0900

    version 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Sliderify
+sliderify
 ====
 
 Make presentation slide. Easily.
@@ -6,28 +6,28 @@ Make presentation slide. Easily.
 ## Usage
 
 * Copy sample to your_slide_name
-* Edit 'your_slide_name/text.js'
-* Open URL file:///path/to/sliderify/index.html?slide=your_slide_name
+* Edit 'your_slide_name/md.js'
+* Open URL file:///path/to/your/sliderify/index.html?slide=your_slide_name
 
-text.js
+md.js
 ```
-define(function() {return function() {/*
+d=_=>{/*
 
 # Your Awesome Slide
 
 ----
 
-# [Sliderify](https://github.com/yatemmma/Sliderify)
+## [sliderify](https://github.com/yatemmma/sliderify)
 
 Love Slide,    
 Love Markdown
 
-*/}}); // Do not remove first and last row.
+*/} // Do not remove first and last row.
 ```
 
 ## Sample
 
-http://yatemmma.github.io/Sliderify/
+http://yatemmma.github.io/sliderify/
  
 ## License
 
---
+++ sample/md.js
@@ -0,0 +1,72 @@
+d=_=>{/*
+
+# My Awesome Slide
+
+----
+
+## sliderify
+**"sliderify"** is a tool to create a simple and beautiful slide easily by markdown.
+
+----
+
+## Points
+
+* Simple
+* Beautiful
+* Easy
+* Markdown
+
+----
+
+### Press "p" key
+
+Progress bar like [Rabbit](https://rabbit-shocker.org/) displayed.
+
+----
+
+### Press "f" key
+
+Full screen.
+
+----
+
+### Load your file
+
+file:///path/to/your/sliderify/index.html?slide=your_slide
+
+----
+
+### URL Parameters
+
+Key      | Value                  | Default
+---------|------------------------|--------
+slide    | directory name         | sample
+progress | show progress bar      | false
+limit    | progress limit minutes | 5
+time     | progress start minutes | 0
+
+----
+
+### Custom CSS
+
+write _custom.css_
+
+```
+ #p8 {
+   background-image: url(background-image.jpg);
+   background-size: cover;
+   color: darkred;
+ }
+````
+
+----
+
+![image](sample/yatemmma.jpeg)
+
+Auther: [yatemmma](https://twitter.com/yatemmma)
+
+----
+
+Powered by [sliderify](https://github.com/yatemmma/sliderify).
+
+*/}
