ffbc47167652e06b9e8abcd83992fcf5078ec20c
yatemmma <yatemmma@gmail.com>
2019-01-26 13:16:42 +0900

    add slide

---
+++ jamstack/md.js
@@ -0,0 +1,220 @@
+d=_=>{/*
+
+# JAMStackで遊んでみた
+
+----
+
+![image](sample/yatemmma.jpeg)
+
+[@yatemmma](https://twitter.com/yatemmma)
+
+----
+
+# JAMStackで遊んでみた
+
+----
+
+# JAMStackで遊んでみたかった
+
+----
+
+# JAMStackで遊んでみたい
+
+----
+
+# JAMStackとは
+
+[https://jamstack.org/](https://jamstack.org/)
+
+----
+
+# 色々なStack
+
+----
+
+## AMP Stack
+
+### Apache / MySQL / PHP
+
+----
+
+## MEAN Stack
+
+### MongoDB / Express / Angular / Node.js
+
+----
+
+## FF Stack
+
+### Elixir / Phoenix / Mithril
+
+----
+
+## JAMStack
+
+## JavaScript / APIs / Markup
+
+----
+
+> Modern web development architecture based on client-side JavaScript, reusable APIs, and prebuilt Markup.
+
+----
+
+### client-side JavaScript
+
+SPAフレームワークからVanilla JSまで
+
+----
+
+### reusable APIs
+
+データの取得やサーバサイドの処理はJavaScriptからAPI経由で
+
+----
+
+### prebuilt Markup
+
+静的サイトジェネレーターを活用
+
+----
+
+### JAMStack => 静的サイトジェネレータで作るWeb周りの仕組みがモダンな感じになってきたので新旧含めてJAMStackと呼んじゃおう！名前なかったしね。
+
+----
+
+## 静的サイトジェネレーター
+[https://www.staticgen.com/](https://www.staticgen.com/)
+
+----
+
+## 静的サイトジェネレーター
+
+### データ（CMS） + テンプレート => ビルド => デプロイ
+
+----
+
+## 人気なもの（だったもの）
+
+----
+
+![Movable Type](https://www.sixapart.jp/share/images/ogimage-mt.png)
+
+Perl製、独自タグ、有償、企業サイトなど
+
+----
+
+![WordPress](https://plugmize.jp/wp-content/uploads/2016/08/wp-thumb.png)
+
+PHP製、独自タグ、OSS
+
+----
+
+![Jekyll](https://jekyllrb-ja.github.io/img/logo-2x.png)
+
+Ruby製、Markdownから生成、GitHub Pages
+
+----
+
+![Hugo](https://raw.githubusercontent.com/gohugoio/hugoDocs/master/static/img/hugo-logo.png)
+
+Go製、OSSリファレンスやブログなど
+
+----
+
+## モダンなJAMStack構成
+
+### Headless CMS + テンプレート =>
+### ビルド => デプロイ（CDN/サーバーレス）
+
+----
+
+## Headless CMS
+
+Webサービスとして提供されているものが多い。CaaS（Content as a Service） 　　　
+
+----
+
+![Contentful](https://camo.qiitausercontent.com/ffc4c5781a95c18c8cf354cc0261533e54522bfe/68747470733a2f2f71696974612d696d6167652d73746f72652e73332e616d617a6f6e6177732e636f6d2f302f3132333432382f65323161393830642d306261342d613265642d366230302d3865343939646534616561312e706e67)
+
+自由度高い、データの定義とバリデーション設定で簡単なRDB、GraphQLで提供、free planで5,000レコード
+
+----
+
+24 Headless CMS That Should Be On Your Radar in 2019
+
+[https://www.cmswire.com/web-cms/13-headless-cmss-to-put-on-your-radar/](https://www.cmswire.com/web-cms/13-headless-cmss-to-put-on-your-radar/)
+
+サービスはたくさんある。自分でDeployできるタイプはなさそう
+
+----
+
+### ビルドツール
+
+データソースがHeadless CMSに対応、生成するサイトもReactやVueに対応
+
+----
+
+![GatsbyJS](https://camo.githubusercontent.com/5f9c7d37f3a89b6aac6e8b9fc577aa5dcf6fc8e5/68747470733a2f2f64337676366c703535716a6171632e636c6f756466726f6e742e6e65742f6974656d732f3434324d32693068317a303532613436305333682f6761747362792d6c6f676f2e706e673f582d436c6f75644170702d56697369746f722d49643d343336343226763d6365646335633663)
+
+React、PWA, prerender
+
+----
+
+### 他にも色々
+
+### Next.js, React Static
+
+### Nuxt.js, VuePress
+
+----
+
+### デプロイ先
+
+CDNや独自ドメイン、httpsなど細かいところまでサポートが増えた。無料で使えて何も考えずに利用しても恩恵にあずかれる
+
+----
+
+![Netlify](https://connpass-tokyo.s3.amazonaws.com/thumbs/47/39/4739b305347f19e3757adcddbda1623f.png)
+
+https、CSN、スケーラブル、Form、AWS Lambdaの呼び出し、Netlifyの一強な感じ、[読み方](https://twitter.com/keiko713/status/996932974930100224)
+
+----
+
+## 組み合わせ
+
+[https://jamstack.org/examples/](https://jamstack.org/examples/)
+
+----
+
+## JAMStackのメリット
+
+高速化・スケーリング・セキュリティ・Developer Experience
+
+
+----
+
+## まとめ
+
+* 向いているweb構築にはどんどん採用しよう
+* Jekyll+GitHub Pagesで十分な人は何もいらない
+* シンプルなサイトのパフォーマンスが上がると、大規模動的サイトにはプレッシャーになりそう
+* あまりお金が動く分野ではないから盛り上がりは微妙かも
+
+----
+
+# JAMStackで遊んでみた
+
+----
+
+# JAMStackで遊んでみたかった
+
+----
+
+# JAMStackで遊んでみたい
+
+----
+
+# Netlifyすごい
+
+
+*/}
